970480d25e8fa83c6272edcd35d63e4204dbf709
Boris Vinogradov <no111u3@gmail.com>
2019-10-18 12:54:59 +0300

    Update project skeleton to App-based form

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 
-#[get("/")]
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+struct AppState {
+    app_name: String,
+}
+
+fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+
+    format!("Hello from {}!", app_name)
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.data(AppState {
+        app_name: String::from("Draft Application of Remote Control"),
+    })
+    .route("/", web::get().to(index));
 }
 
 fn main() {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().configure(config))
         .bind("127.0.0.1:8088")
         .unwrap()
         .run()
